c4203e26fcf40416ac0f0e2ea50d71ecde521b4d
RomanLaypanov <rrrrraaammmmaaaazzz@gmail.com>
2025-07-19 12:55:31 +0300

    задача 6.2.15 метод возвращает BigInteger

--- src/FactorialCalculator.java
+++ src/FactorialCalculator.java
@@ -3,12 +3,11 @@ import java.math.BigInteger;
 import static java.util.stream.IntStream.rangeClosed;
 
 public class FactorialCalculator {
-    public static void factorial(int i) {
+    public static BigInteger factorial(int i) {
         if (i < 0) {
             throw new IllegalArgumentException("отрицательное число");
         }
 
-        BigInteger bI = rangeClosed(1, i).mapToObj(BigInteger::valueOf).reduce(BigInteger::multiply).get();
-        System.out.println(bI);
+        return rangeClosed(1, i).mapToObj(BigInteger::valueOf).reduce(BigInteger::multiply).get();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        FactorialCalculator.factorial(0);
+        System.out.println(FactorialCalculator.factorial(0));
     }
 }
